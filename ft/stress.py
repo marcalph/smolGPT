4a05ee62794ae2d5657bb76b28ff6513e7c704ce
marcalph <marcalph@protonmail.com>
2024-12-05 09:44:09 +0100

    eval logic

--- ft/stress.py
+++ ft/stress.py
@@ -7,12 +7,12 @@ import logging
 import numpy as np
 from dotenv import load_dotenv
 from pathlib import Path
-from pprint import pprint
-
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from tqdm import tqdm
 load_dotenv("/Users/marcalph/.ssh/llm_api_keys.env")
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+logging.getLogger('httpx').setLevel(logging.WARNING)
 
 def load_stress_data(raw_path:Path) -> pd.DataFrame:
   data = pd.read_csv(raw_path/"Reddit_Title.csv", sep=';')
@@ -39,10 +39,13 @@ def df_to_jsonl(data:pd.DataFrame, output_file_path:Path) -> None:
   logger.info(f"Saved {len(jsonl_data)} items to {output_file_path.name}")
 
 
-def split_and_serialize_to_jsonl(df:pd.DataFrame, output_dir:Path, fragment_trainset:bool=True) -> None:
+def split_and_serialize_to_jsonl(df:pd.DataFrame, output_dir:Path, serialize:bool=True, fragment_trainset:bool=True) -> None:
   # split data into training and validation
   train_data, validation_data = train_test_split(df, test_size=0.2, random_state=42)
   
+  if not serialize:
+    return train_data, validation_data
+    
   # serialize data
   os.makedirs(output_dir, exist_ok=True)
 
@@ -57,6 +60,7 @@ def split_and_serialize_to_jsonl(df:pd.DataFrame, output_dir:Path, fragment_trai
   validation_output_file_path = output_dir/'stress_detection_validation.jsonl'
   df_to_jsonl(validation_data, validation_output_file_path)
   logger.info(f"jsonl files saved to {output_dir}")
+  return train_data, validation_data
 
 
 
@@ -93,21 +97,104 @@ def finetune_test(openai_client:OpenAI, model="gpt-3.5-turbo") -> str:
   return job_id
 
 
+def predict(openai_client: OpenAI, test_df: pd.DataFrame,  model:str):
+    y_pred = []
+    categories = ["non-stress", "stress"]
+    for _, row in tqdm(test_df.iterrows(), total=test_df.shape[0], desc="Processing rows"):
+        response = openai_client.chat.completions.create(
+            model=model,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "Given a social media post, classify whether it indicates 'stress' or 'non-stress'.",
+                },
+                {"role": "user", "content": row["title"]},
+            ],
+        )
+
+        answer = response.choices[0].message.content
+
+        for category in categories:
+            if category.lower() in answer.lower():
+                y_pred.append(category)
+                break
+        else:
+            y_pred.append("none")
+    return y_pred
+
+
+
+def run_evaluation(y_true: np.ndarray, y_pred:np.ndarray):
+    labels = ["non-stress", "stress"]
+    mapping = {label: idx for idx, label in enumerate(labels)}
+
+    def map_func(x):
+        return mapping.get(
+            x, -1
+        )  # Map to -1 if not found, but should not occur with correct data
+
+    y_true_mapped = np.vectorize(map_func)(y_true)
+    y_pred_mapped = np.vectorize(map_func)(y_pred)
+
+    # Calculate accuracy
+
+    accuracy = accuracy_score(y_true=y_true_mapped, y_pred=y_pred_mapped)
+    print(f"Accuracy: {accuracy:.3f}")
+
+    # Generate accuracy report
+
+    unique_labels = set(y_true_mapped)  # Get unique labels
+
+    for label in unique_labels:
+        label_indices = [
+            i for i in range(len(y_true_mapped)) if y_true_mapped[i] == label
+        ]
+        label_y_true = [y_true_mapped[i] for i in label_indices]
+        label_y_pred = [y_pred_mapped[i] for i in label_indices]
+        label_accuracy = accuracy_score(label_y_true, label_y_pred)
+        print(f"Accuracy for label {labels[label]}: {label_accuracy:.3f}")
+    # Generate classification report
+
+    class_report = classification_report(
+        y_true=y_true_mapped,
+        y_pred=y_pred_mapped,
+        target_names=labels,
+        labels=list(range(len(labels))),
+    )
+    print("\nClassification Report:")
+    print(class_report)
+
+    conf_matrix = confusion_matrix(
+        y_true=y_true_mapped, y_pred=y_pred_mapped, labels=list(range(len(labels)))
+    )
+    print("\nConfusion Matrix:")
+    print(conf_matrix)
+
+
+
+
+
 if __name__ == "__main__":
   from utils import STRESS_RAW_PATH
+  output_dir = STRESS_RAW_PATH.parent/"processed"
   client = OpenAI(api_key=os.getenv("OPENAI_API_KEY_MLEXPERIMENTS"))
   prepare_data = False
   train = False
-  evaluate = False
+  evaluate = True
 
   if prepare_data:
     # load data
     data_cleaned = load_stress_data(STRESS_RAW_PATH)
-    output_dir = STRESS_RAW_PATH.parent/"processed"
     # process data
-    split_and_serialize_to_jsonl(data_cleaned, output_dir, fragment_trainset=True)
+    split_and_serialize_to_jsonl(data_cleaned, output_dir, serialize=True, fragment_trainset=True)
     # upload
     upload_datasets(output_dir, client)
   if train:
     finetune_test(client)
-  
+  if evaluate:
+    data_cleaned = load_stress_data(STRESS_RAW_PATH)
+    _, test_df = split_and_serialize_to_jsonl(data_cleaned, output_dir, serialize=False)
+    y_pred = predict(client, test_df, model="gpt-3.5-turbo")  
+    run_evaluation(test_df['label'], y_pred)
+    y_pred = predict(client, test_df, model="ft:gpt-3.5-turbo-0125:wingmate::AarlN1qI")  
+    run_evaluation(test_df['label'], y_pred)
